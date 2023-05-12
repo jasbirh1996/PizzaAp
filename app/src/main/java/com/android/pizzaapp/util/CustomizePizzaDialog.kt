49060daa0f39e6f75030eab5089720151a659acb
Jasbir <jasbirh1996@gmail.com>
2023-05-13 03:12:43 +0530

    Customize Pizza Dialog is built

--- app/src/main/java/com/android/pizzaapp/util/CustomizePizzaDialog.kt
+++ app/src/main/java/com/android/pizzaapp/util/CustomizePizzaDialog.kt
@@ -5,19 +5,23 @@ import android.content.Context
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.util.Log
 import android.view.Gravity
+import android.view.View
 import android.view.View.inflate
 import android.view.ViewGroup
 import android.view.Window
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import com.android.pizzaapp.R
 import com.android.pizzaapp.databinding.CustomizePizzaLayoutBinding
 import java.lang.ref.WeakReference
 
 
 class CustomizePizzaDialog(context: Context, private var listener: AddToCartListener) : Dialog(context, R.style.Theme_Dialog) {
-
+    val binding = CustomizePizzaLayoutBinding.inflate(layoutInflater)
     override fun onCreate(savedInstanceState: Bundle?) {
-        val binding = CustomizePizzaLayoutBinding.inflate(layoutInflater)
+
         super.onCreate(savedInstanceState)
         requestWindowFeature(Window.FEATURE_NO_TITLE)
         setContentView(binding.root)
@@ -28,11 +32,61 @@ class CustomizePizzaDialog(context: Context, private var listener: AddToCartList
         wlp.gravity = Gravity.BOTTOM
         window.attributes = wlp
         window.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        setupCrustSpinner(arrayListOf("pizza","size"))
+        setUpSizeSpinner(arrayListOf("5","4"))
+
         binding.button.setOnClickListener {
             listener.addToCart()
             dismiss()
         }
 
+    }
+    private fun setupCrustSpinner(list : ArrayList<String>){
+        val adapter = ArrayAdapter(
+            context,
+            android.R.layout.simple_spinner_item,
+            list,
+
+        )
+
+        binding.spCrust.adapter = adapter
+        binding.spCrust.setSelection(1)
+
+        binding.spCrust.onItemSelectedListener = object :AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(parent: AdapterView<*>?, p1: View?, position: Int, p3: Long) {
+                // logic on selection
+                var data = parent?.getItemAtPosition(position) as String
+                Log.e("spinnerCrust",data)
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+                // do nothing
+            }
+
+        }
+    }
+
+    private fun setUpSizeSpinner(list : ArrayList<String>){
+        val adapter = ArrayAdapter(
+            context,
+            android.R.layout.simple_spinner_item,
+            list,
+
+            )
+
+        binding.spSize.adapter = adapter
+        binding.spSize.onItemSelectedListener = object :AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(parent: AdapterView<*>?, p1: View?, position: Int, p3: Long) {
+             // logic on selection
+                var data = parent?.getItemAtPosition(position) as String
+                Log.e("spinnerSize",data)
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+                // do nothing
+            }
+
+        }
     }
 }
 interface AddToCartListener {
